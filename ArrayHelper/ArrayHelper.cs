d31382ac303df99054af2a6efe5ad5327f9ee550
larisa33 <muntoiu.larisa@gmail.com>
2023-10-22 12:37:08 +0300

    min si max recursiv

---
+++ ArrayHelper/ArrayHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayHelper
+{
+
+    public static class ArrayHelper
+    {
+        public static int FindMinRecursive(int[] array, int index, int minim)
+        {
+            if (index == array.Length)
+            {
+                return minim;
+            }
+
+            if (array[index] < minim)
+            {
+                minim = array[index];
+            }
+
+            return FindMinRecursive(array, index+1, minim);
+        }
+
+        public static int FindMaxRecursive(int[] array, int index, int minim)
+        {
+            if (index == array.Length)
+            {
+                return minim;
+            }
+
+            if (array[index] > minim)
+            {
+                minim = array[index];
+            }
+
+            return FindMaxRecursive(array, index + 1, minim);
+        }
+    }
+}
+
--- ArrayHelper/Program.cs
+++ ArrayHelper/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Introdu numarul de elemente ale array-ului: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.Write("Array-ul este: { ");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+
+            Console.WriteLine("}");
+
+            int min = ArrayHelper.FindMinRecursive(numbers, 0, numbers[0]);
+
+            Console.WriteLine("Minimul din array-ul dat este: " + min);
+
+            int max = ArrayHelper.FindMaxRecursive(numbers, 0, numbers[0]);
+
+            Console.WriteLine("Maximul din array-ul dat este: " + max);
         }
     }
 }
